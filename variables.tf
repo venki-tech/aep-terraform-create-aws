a136076e5cb2c10145af39fc56ea5b9ded81c117
venki-tech <32933738+venki-tech@users.noreply.github.com>
2020-10-08 07:58:18 +0100

    new

--- variables.tf
+++ variables.tf
@@ -2,3 +2,16 @@ variable "region" {
   default = "eu-west-2"
 }
 
+variable "Ubuntu_AMIS" {
+  type = "map"
+  default = {
+    eu-west-2 = "ami-0fb673bc6ff8fc282"
+  }
+}
+
+variable "RH_AMIS" {
+  type = "map"
+  default = {
+    eu-west-2 = "ami-0fc841be1f929d7d1"
+  }
+}
