6c70712cb41984c5344fb3833efd40f794d9c600
venki-tech <32933738+venki-tech@users.noreply.github.com>
2020-10-12 12:01:57 +0100

    new

--- createInventory.sh
+++ createInventory.sh
@@ -4,11 +4,11 @@ echo "-------------------------------------"
 rm -rf ${WORKSPACE}/temp_repo_ws
 cd ${WORKSPACE}/aep-terraform-create-aws/
 git remote set-url origin git@github.com:venki-tech/aep-terraform-create-aws.git
-git add terraform.tfstate* inventory.txt
+git add terraform.tfstate*
 git commit -m "Added terraform state files to repo" || true
 git push origin HEAD:master
 mkdir -p ${WORKSPACE}/temp_repo_ws
-cp terraform.tfstate* inventory.txt ${WORKSPACE}/temp_repo_ws
+cp terraform.tfstate* inventory.txt supply_hosts.txt ${WORKSPACE}/temp_repo_ws
 rm -rf ${WORKSPACE}/aep-terraform-create-aws
 
 echo "-------------------------------------"
@@ -17,7 +17,7 @@ git clone git@github.com:venki-tech/aws-terraform.git
 cd ${WORKSPACE}/aws-terraform
 git pull
 cp ${WORKSPACE}/temp_repo_ws/* .
-git add terraform.tfstate* inventory.txt
+git add terraform.tfstate*
 git commit -m "Added terraform state files to repo" || true
 git push origin HEAD:master
 rm -rf ${WORKSPACE}/aws-terraform
@@ -28,16 +28,21 @@ git clone git@github.com:venki-tech/aep-ansible-provision.git
 cd ${WORKSPACE}/aep-ansible-provision
 git pull
 if [[ -f runninginventory.txt ]];then
-  echo "runninginventory.txt file exists. Will add the new hosts into it."
-  cp ${WORKSPACE}/temp_repo_ws/inventory.txt .
+  echo "Check if the servers already exists, if yes do not add it to runninginventory.txt"
+  cp ${WORKSPACE}/temp_repo_ws/supply_hosts.txt .
+  exists=$(grep hosts_exists supply_hosts.txt|cut -f2 -d'=')
+  if [[ $exists == "no" ]];then
+      echo "runninginventory.txt file exists and the hosts are newly provisioned. Will add the new hosts into it."
+      cp ${WORKSPACE}/temp_repo_ws/inventory.txt .
 
-  perl -0777 -nle 'print $1 if m/(\[all\](.|\n|\r)*)\[aws_instances/g' runninginventory.txt >> newrunninginventory.txt
-  perl -0777 -nle 'print $1 if m/all\]((.|\n|\r)*)\[aws_instances/g' inventory.txt >> newrunninginventory.txt
-  perl -0777 -nle 'print $1 if m/(\[aws_instances\](.|\n|\r)*)/g' runninginventory.txt  >> newrunninginventory.txt
-  perl -0777 -nle 'print $1 if m/aws_instances\]((.|\n|\r)*)/g' inventory.txt >> newrunninginventory.txt
+      perl -0777 -nle 'print $1 if m/(\[all\](.|\n|\r)*)\[aws_instances/g' runninginventory.txt >> newrunninginventory.txt
+      perl -0777 -nle 'print $1 if m/all\]((.|\n|\r)*)\[aws_instances/g' inventory.txt >> newrunninginventory.txt
+      perl -0777 -nle 'print $1 if m/(\[aws_instances\](.|\n|\r)*)/g' runninginventory.txt  >> newrunninginventory.txt
+      perl -0777 -nle 'print $1 if m/aws_instances\]((.|\n|\r)*)/g' inventory.txt >> newrunninginventory.txt
 
-  mv -f newrunninginventory.txt runninginventory.txt
-  rm -f inventory.txt
+      mv -f newrunninginventory.txt runninginventory.txt
+      rm -f inventory.txt
+  fi
 else
   echo "Running inventory file doesnt exist. Will just rename inventory file as runninginventory file"
   cp ${WORKSPACE}/temp_repo_ws/inventory.txt .
